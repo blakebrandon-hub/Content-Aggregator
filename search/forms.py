7e28e81270bb0ee030aefdff1b8f05e291a33f76
blakebrandon-hub <50201165+blakebrandon-hub@users.noreply.github.com>
2021-03-03 22:26:52 -0600

    Delete search directory

--- search/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Query
-
-class SearchForm(forms.ModelForm):
-	class Meta:
-		model = Query
-		fields = '__all__'
-		
--- search/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Query(models.Model):
-	search = models.CharField(max_length=250)
